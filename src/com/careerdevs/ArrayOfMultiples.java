1533a3a424b75d58389d1ec2ffff34bc482378d1
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-08-12 18:18:37 -0400

    added project

---
+++ src/com/careerdevs/ArrayOfMultiples.java
@@ -0,0 +1,9 @@
+package com.careerdevs;
+
+public class ArrayOfMultiples {
+
+
+    public static void solution(int num,int length){
+
+    }
+}
---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,20 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Array of Multiples: https://edabit.com/challenge/rzpucPyoyEtXPo2BG
+        ArrayOfMultiples.solution(7, 10);
+
+        // Perfect Square Patch: https://edabit.com/challenge/7Tb7qMDQHtz3xpydd
+
+
+        // Reverse the String (with a catch): https://edabit.com/challenge/QdrLgb37ZZwYMKXwi
+
+
+        // Atbash Cipher: https://edabit.com/challenge/JmuM2cP5MvruRjr6c
+
+
+    }
+}
