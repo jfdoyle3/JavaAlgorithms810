2589c249b2cdc7ceebf5c51cb0650064b6015eed
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-08-13 06:55:20 -0400

    completed ArrayOfMultiples / Perfect Square Patch

--- src/com/careerdevs/ArrayOfMultiples.java
+++ src/com/careerdevs/ArrayOfMultiples.java
@@ -1,9 +1,17 @@
 package com.careerdevs;
 
+import java.util.Arrays;
+
 public class ArrayOfMultiples {
 
 
     public static void solution(int num,int length){
-
+        int[] multiples=new int[length];
+        int counter=1;
+        for (int idx=0; idx<length; idx++) {
+            multiples[idx] = num * (idx+counter);
+            System.out.println(multiples[idx]);
+        }
+        System.out.println(Arrays.toString(multiples));
     }
 }
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         // Array of Multiples: https://edabit.com/challenge/rzpucPyoyEtXPo2BG
-        ArrayOfMultiples.solution(7, 10);
+        ArrayOfMultiples.solution(7, 5);
 
         // Perfect Square Patch: https://edabit.com/challenge/7Tb7qMDQHtz3xpydd
-
+        PerfectSquarePatch.solution(3);
 
         // Reverse the String (with a catch): https://edabit.com/challenge/QdrLgb37ZZwYMKXwi
 
