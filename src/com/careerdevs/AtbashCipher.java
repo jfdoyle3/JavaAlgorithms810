2efa37ef56123cdd59698b761bad36811abf7da2
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-08-14 18:20:50 -0400

    almost have AtbashCipher completed

--- src/com/careerdevs/AtbashCipher.java
+++ src/com/careerdevs/AtbashCipher.java
@@ -1,6 +1,7 @@
 package com.careerdevs;
 
 public class AtbashCipher {
+
     // 97 = a   -/+ 32    65 = A
     // 122 = z   -/+ 32   90 = Z
     // a-z=25             A-Z=25
@@ -9,20 +10,46 @@ public class AtbashCipher {
     // a=1   z=25;
     //
     //
+    private static int ascii, oppLetter;
+    private static char letter;
+
     public static void solution(String input) {
+        StringBuilder cipher = new StringBuilder();
+
         char[] inputArr = new char[input.length()];
         for (int i = 0; i < input.length(); i++)
             inputArr[i] = input.charAt(i);
 
-        int ascii;
-        char letter;
-        for (char c : inputArr) {
-           ascii = c - 26;
-          letter = (char) ascii;
-            System.out.print(letter+" ");
+        for(char c : inputArr){
+            if(c>='A' && c<='Z')
+              cipher.append(upperCaseLetter(c));
+
+            if(c>='a' && c<='z')
+                cipher.append(lowerCaseLetter(c));
+//            else
+//                cipher.append(c);
+
         }
+
+        System.out.println(cipher.toString());
 
 
 
     }
-}
+
+        private static char lowerCaseLetter ( char c){
+
+            ascii = c - 97;
+            oppLetter = 26 - ascii + 96;
+            letter = (char) oppLetter;
+            return letter;
+        }
+
+        private static char upperCaseLetter (char c){
+
+            ascii = c - 65;
+            oppLetter = 26 - ascii + 64;
+            letter = (char) oppLetter;
+            return letter;
+        }
+    }
